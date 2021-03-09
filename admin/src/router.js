a14c00d62943fad767611254d3449e740c22f9ef
wzf <80504767@qq.com>
2021-03-09 19:15:55 +0800

    创建admin,优化admin和login逻辑

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,16 +1,24 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Login from './views/Login.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "./views/Login.vue";
+import Admin from "./views/Admin.vue";
 
 Vue.use(Router);
 export default new Router({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes:[{
-        path: '*',
-        redirect: '/login',
-    },{
-        path: '/login',
-        component: Login
-    }]
-})
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "*",
+      redirect: "/login",
+    },
+    {
+      path: "/login",
+      component: Login,
+    },
+    {
+      path: '/admin',
+      component: Admin
+    }
+  ],
+});
