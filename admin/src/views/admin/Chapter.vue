72cf276ba19245846dff52105bfad199cf466362
wangzhifang <2392756129@qq.com>
2021-03-12 19:26:23 +0800

    增加刷新按鈕，並添加刷新功能

--- admin/src/views/admin/Chapter.vue
+++ admin/src/views/admin/Chapter.vue
@@ -1,135 +1,143 @@
 <template>
+  <div>
+    <p>
+      <!-- 刷新按钮 -->
+      <button class="btn btn-white btn-default btn-round" @click="list()"> 
+        <i class="ace-icon fa fa-refresh"></i>刷 新
+      </button>
+    </p>
     <!-- 表格 -->
     <table id="simple-table" class="table  table-bordered table-hover">
-    <thead>
+      <thead>
         <tr>
-            <th class="detail-col">ID</th>
-            <th>名称</th>
-            <th>课程ID</th>
-            <th class="hidden-480">操作</th>
-        </tr>    
-    </thead>
-
-    <tbody>
-       <tr v-for="chapter in chapters" v-bind:key="chapter.id">
-            <td>{{chapter.id}}</td>
-            <td>{{chapter.name}}</td>
-            <td>{{chapter.courseId}}</td>
-            <td>
-                <div class="hidden-sm hidden-xs btn-group">
-                <button class="btn btn-xs btn-success">
-                    <i class="ace-icon fa fa-check bigger-120"></i>
-                </button>
-
-                <button class="btn btn-xs btn-info">
-                    <i class="ace-icon fa fa-pencil bigger-120"></i>
-                </button>
-
-                <button class="btn btn-xs btn-danger">
-                    <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                </button>
-
-                <button class="btn btn-xs btn-warning">
-                    <i class="ace-icon fa fa-flag bigger-120"></i>
-                </button>
-                </div>
-
-                <div class="hidden-md hidden-lg">
-                <div class="inline pos-rel">
-                    <button
-                    class="btn btn-minier btn-primary dropdown-toggle"
-                    data-toggle="dropdown"
-                    data-position="auto"
-                    >
-                    <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                    </button>
-
-                    <ul
-                    class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close"
-                    >
-                    <li>
-                        <a
-                        href="#"
-                        class="tooltip-info"
-                        data-rel="tooltip"
-                        title="View"
-                        >
-                        <span class="blue">
-                            <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                        </span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a
-                        href="#"
-                        class="tooltip-success"
-                        data-rel="tooltip"
-                        title="Edit"
-                        >
-                        <span class="green">
-                            <i
-                            class="ace-icon fa fa-pencil-square-o bigger-120"
-                            ></i>
-                        </span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a
-                        href="#"
-                        class="tooltip-error"
-                        data-rel="tooltip"
-                        title="Delete"
-                        >
-                        <span class="red">
-                            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                        </span>
-                        </a>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </td>
+          <th class="detail-col">ID</th>
+          <th>名称</th>
+          <th>课程ID</th>
+          <th class="hidden-480">操作</th>
         </tr>
-    </tbody>
-    </table>
-  
-</template>
+      </thead>
 
+      <tbody>
+        <tr v-for="chapter in chapters" v-bind:key="chapter.id">
+          <td>{{ chapter.id }}</td>
+          <td>{{ chapter.name }}</td>
+          <td>{{ chapter.courseId }}</td>
+          <td>
+            <div class="hidden-sm hidden-xs btn-group">
+              <button class="btn btn-xs btn-success">
+                <i class="ace-icon fa fa-check bigger-120"></i>
+              </button>
+
+              <button class="btn btn-xs btn-info">
+                <i class="ace-icon fa fa-pencil bigger-120"></i>
+              </button>
+
+              <button class="btn btn-xs btn-danger">
+                <i class="ace-icon fa fa-trash-o bigger-120"></i>
+              </button>
+
+              <button class="btn btn-xs btn-warning">
+                <i class="ace-icon fa fa-flag bigger-120"></i>
+              </button>
+            </div>
+
+            <div class="hidden-md hidden-lg">
+              <div class="inline pos-rel">
+                <button
+                  class="btn btn-minier btn-primary dropdown-toggle"
+                  data-toggle="dropdown"
+                  data-position="auto"
+                >
+                  <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+                </button>
+
+                <ul
+                  class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      class="tooltip-info"
+                      data-rel="tooltip"
+                      title="View"
+                    >
+                      <span class="blue">
+                        <i class="ace-icon fa fa-search-plus bigger-120"></i>
+                      </span>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      class="tooltip-success"
+                      data-rel="tooltip"
+                      title="Edit"
+                    >
+                      <span class="green">
+                        <i
+                          class="ace-icon fa fa-pencil-square-o bigger-120"
+                        ></i>
+                      </span>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      class="tooltip-error"
+                      data-rel="tooltip"
+                      title="Delete"
+                    >
+                      <span class="red">
+                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
 
 <script>
 export default {
   name: "chapter",
   //返回值
-  data: function(){
-    return{
-        chapters:[]
-    }  
+  data: function() {
+    return {
+      chapters: [],
+    };
   },
-  mounted: function(){
+  mounted: function() {
     let _this = this;
     _this.list();
   },
   methods: {
     //大章列表
     list() {
-        let _this = this;
-        _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/list",
-        //传参对象
-        {
-            pageSize:1,
-           currentPage:1
-        }).then(
-            
-            //响应结果
-            (response)=>{
-                console.log("查询大章列表",response);
-                _this.chapters = response.data.records;
-            }
+      let _this = this;
+      _this.$ajax
+        .post(
+          "http://127.0.0.1:9000/business/admin/chapter/list",
+          //传参对象
+          {
+            pageSize: 1,
+            currentPage: 1,
+          }
         )
-    }
-  }
+        .then(
+          //响应结果
+          (response) => {
+            console.log("查询大章列表", response);
+            _this.chapters = response.data.records;
+          }
+        );
+    },
+  },
 };
 </script>
-
