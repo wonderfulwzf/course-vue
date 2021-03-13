c71f5db99270e239e14a3be54de591edf129db56
wzf <80504767@qq.com>
2021-03-13 13:41:20 +0800

    实现添加大章还有分页功能

--- admin/src/views/admin/Chapter.vue
+++ admin/src/views/admin/Chapter.vue
@@ -7,10 +7,12 @@
       </button>
       &nbsp;
       <!-- 刷新按钮 -->
-      <button class="btn btn-white btn-default btn-round" @click="list()">
+      <button class="btn btn-white btn-default btn-round" @click="list(1)">
         <i class="ace-icon fa fa-refresh"></i>刷 新
       </button>
     </p>
+    <!-- 分页 -->
+    <pagination ref="pagination" v-bind:list="list"></pagination>
     <!-- 表格 -->
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
@@ -133,6 +135,7 @@
                     type="text"
                     class="form-control"
                     placeholder="名称"
+                    v-model="chapter.name"
                   />
                 </div>
               </div>
@@ -145,6 +148,7 @@
                     type="text"
                     class="form-control"
                     placeholder="课程ID"
+                    v-model="chapter.courseId"
                   />
                 </div>
               </div>
@@ -154,7 +158,7 @@
             <button type="button" class="btn btn-default" data-dismiss="modal">
               取消
             </button>
-            <button type="button" class="btn btn-primary">保存</button>
+            <button type="button" class="btn btn-primary" v-on:click="save()">保存</button>
           </div>
         </div>
       </div>
@@ -164,41 +168,71 @@
 </template>
 
 <script>
+import Pagination from '../../components/Pagination.vue';
 export default {
+ components: { Pagination },
   name: "chapter",
   //返回值
   data: function() {
     return {
+      chapter:{},
       chapters: [],
     };
   },
   mounted: function() {
     let _this = this;
-    _this.list();
+    _this.$refs.pagination.size = 5;
+    _this.list(1);
   },
   methods: {
-    //添加大章
+    //添加大章打开模态框
     add() {
       //let _this = this;
       $(".modal").modal("show");
     },
     //大章列表
-    list() {
+    list(page) {
       let _this = this;
       _this.$ajax
         .post(
           "http://127.0.0.1:9000/business/admin/chapter/list",
           //传参对象
           {
-            pageSize: 10,
-            currentPage: 1,
+            pageSize: _this.$refs.pagination.size,
+            currentPage: page,
           }
         )
         .then(
           //响应结果
           (response) => {
             console.log("查询大章列表", response);
-            _this.chapters = response.data.records;
+            let resp = response.data;
+            _this.chapters = resp.data.records;
+            _this.$refs.pagination.render(page,resp.data.totalRecord);
+          }
+        );
+    },
+    //大章save
+    save() {
+      let _this = this;
+      _this.$ajax
+        .post(
+          "http://127.0.0.1:9000/business/admin/chapter/save",
+          //传参对象
+            _this.chapter
+        )
+        .then(
+          //响应结果
+          (response) => {
+            console.log("保存大章成功", response);
+            let resp = response.data;
+            //保存成功
+            if(resp.success){
+              //关闭模态框
+              $(".modal").modal("hide");
+              //刷新列表
+              _this.list(1);
+            }
           }
         );
     },
