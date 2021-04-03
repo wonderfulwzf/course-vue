3e1ade282d0a5402d76aebb9a6a556ccf987230d
wzf <80504767@qq.com>
2021-04-03 20:43:56 +0800

    优化角色用户打开展示

--- admin/src/views/admin/Role.vue
+++ admin/src/views/admin/Role.vue
@@ -558,7 +558,7 @@ export default {
      let resp = response.data;
      if (resp.success) {
       _this.users = resp.data.records;
-      //_this.listRoleUser();
+      _this.listRoleUser();
      } else {
       Toast.warning(resp.message);
      }
@@ -612,6 +612,33 @@ export default {
       ToastMin.success("保存成功!");
      } else {
       ToastMax.warning(resp.message);
+     }
+    });
+  },
+
+  /**
+   * 加载角色用户
+   */
+  listRoleUser() {
+   let _this = this;
+   _this.roleUsers = [];
+   _this.$ajax
+    .get(
+     process.env.VUE_APP_SERVER +
+      "/system/admin/role/list_user/" +
+      _this.role.id
+    )
+    .then((res) => {
+     let response = res.data;
+     let userIds = response.data;
+
+     // 根据加载到用户ID，到【所有用户数组：users】中查找用户对象，用于列表显示
+     for (let i = 0; i < userIds.length; i++) {
+      for (let j = 0; j < _this.users.length; j++) {
+       if (userIds[i] === _this.users[j].id) {
+        _this.roleUsers.push(_this.users[j]);
+       }
+      }
      }
     });
   },
